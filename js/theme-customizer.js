120a5429476b7d9e74deb1435d8cc0e32be3d74d
themesbros <contact@themesbros.com>
2016-04-14 10:31:05 +0200

    Added support for live preview of separator page title and subtitle.

--- js/theme-customizer.js
+++ js/theme-customizer.js
@@ -5,7 +5,7 @@
 	 */
 	wp.customize( 'blogname', function( value ) {
 		value.bind( function( to ) {
-			if ( ! $( '#site-title img' ).length ) 
+			if ( ! $( '#site-title img' ).length )
 				$( '#site-title a' ).html( to );
 		} ); // value.bind
 	} ); // wp.customize
@@ -18,33 +18,51 @@
 			$( '#site-description' ).html( to );
 		} ); // value.bind
 
-	} ); // wp.customize	
+	} ); // wp.customize
 
 	/*
 	 * Shows a live preview of changing the loop / blog title.
-	 */ 
+	 */
 	wp.customize( 'loop_title', function( value ) {
 		value.bind( function( to ) {
 			$( '.loop-title' ).text( to );
 		});
-	});	 
+	});
 
 	/*
 	 * Shows a live preview of changing the loop / blog title.
-	 */ 
+	 */
 	wp.customize( 'loop_subtitle', function( value ) {
 		value.bind( function( to ) {
 			$( '.loop-description p' ).text( to );
 		});
-	});	 
+	});
+
+	/*
+	 * Shows a live preview of changing the loop / blog title.
+	 */
+	wp.customize( 'loop_title_page', function( value ) {
+		value.bind( function( to ) {
+			$( '.loop-title' ).text( to );
+		});
+	});
+
+	/*
+	 * Shows a live preview of changing the loop / blog title.
+	 */
+	wp.customize( 'loop_subtitle_page', function( value ) {
+		value.bind( function( to ) {
+			$( '.loop-description p' ).text( to );
+		});
+	});
 
 	/*
 	 * Shows a live preview of changing the separator image opacity.
-	 */ 
+	 */
 	wp.customize( 'loop_opacity', function( value ) {
 		value.bind( function( to ) {
 			$( '.loop-img' ).css( 'opacity', to );
 		});
-	});	 
+	});
 
 }( jQuery ));
