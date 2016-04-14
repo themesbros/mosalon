3c67ccab5b0bfaa1d12f7bf90563ec8bdb983ef8
themesbros <contact@themesbros.com>
2016-04-14 10:32:22 +0200

    Added support for page title and subtitle in separator.

--- misc/loop-meta.php
+++ misc/loop-meta.php
@@ -1,25 +1,29 @@
 <?php if ( ! is_mosalon_landing_page() ) : ?>
 
 	<div <?php hybrid_attr( 'loop-meta' ); ?>>
-		
+
 		<?php $img = get_theme_mod( 'loop_img', trailingslashit( get_template_directory_uri() ) . 'images/sep.png' ); ?>
 
 		<?php if ( ! empty( $img ) ) : ?>
 			<div class="loop-img"></div>
 		<?php endif; ?>
 
-		<?php if ( is_front_page() || is_home() || is_singular() ) : 
-			$title =  get_theme_mod( 'loop_title', __( 'From the blog', 'mosalon' ) ); 
-			$desc  = '<p>' . get_theme_mod( 'loop_subtitle', __( 'Latest posts from our blog', 'mosalon' ) ) . '</p>';		
+		<?php if ( is_front_page() || is_page() ) :
+			$title = get_theme_mod( 'loop_title_page', __( 'Company Info', 'mosalon' ) );
+			$desc  = '<p>' . get_theme_mod( 'loop_subtitle_page', __( 'Company slogan', 'mosalon' ) ) . '</p>';
 
-		elseif ( is_404() ) : 
+		elseif ( is_home() || is_singular() ) :
+			$title =  get_theme_mod( 'loop_title', __( 'From the blog', 'mosalon' ) );
+			$desc  = '<p>' . get_theme_mod( 'loop_subtitle', __( 'Latest posts from our blog', 'mosalon' ) ) . '</p>';
+
+		elseif ( is_404() ) :
 			$title = __( 'Nothing Found', 'mosalon' );
 			$desc  = '<p>'. __( 'Apologies, but no entries were found.', 'mosalon' ) . '</p>';
-			
-		else : 
-			$title = esc_attr( get_the_archive_title() ); 
+
+		else :
+			$title = esc_attr( get_the_archive_title() );
 			$desc  = esc_attr( get_the_archive_description() );
-			
+
 			if ( empty( $desc ) ) {
 				// If there's no category description.
 				if ( is_category() ) {
@@ -33,7 +37,7 @@
 				if ( is_tag() )
 					$desc = '<p>' . __( 'You are browsing posts tagged with' , 'mosalon' ) . ' ' . $title . '.</p>';
 			}
-	 
+
 		endif; ?>
 
 		<h1 <?php hybrid_attr( 'loop-title' ); ?>><?php echo $title; ?></h1>
